5093de24fcce8068ceab3f1646c5d25060c2a3a6
KHKAC <hellomrma@gmail.com>
2026-01-25 11:33:24 +0900

    Update

--- Assets/02.Scripts/BamsongiGenerator.cs
+++ Assets/02.Scripts/BamsongiGenerator.cs
@@ -14,7 +14,7 @@ public class BamsongiGenerator : MonoBehaviour
         // 마우스 온쪽 클릭시 밤송이 오브젝트 생성 및 날리기
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject bamsongi = Instantiate(bamsongiPrefab, transform.position, Quaternion.identity);
+            GameObject bamsongi = Instantiate(bamsongiPrefab);
             bamsongi.GetComponent<BamsongiController>().Shoot(new Vector3(0, 250, 2000));
         }
     }
